55b37450c4f40904efdbd7e3e30b32466232006a
msobocinska <msobocinska@sii.pl>
2022-09-01 15:55:05 +0200

    config

--- src/test/java/testbase/TestBase.java
+++ src/test/java/testbase/TestBase.java
@@ -1,4 +1,30 @@
 package testbase;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class TestBase {
+
+    public static WebDriver driver;
+
+    @BeforeAll
+    static void setDriver() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    public void setUp() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+
+    @AfterEach
+    public void tearDown(){
+        driver.quit();
+    }
+
 }
