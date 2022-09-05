93c602562ee5d040956132eae4998206f856305e
msobocinska <msobocinska@sii.pl>
2022-09-05 19:49:35 +0200

    slider

--- src/test/java/testbase/Pages.java
+++ src/test/java/testbase/Pages.java
@@ -4,5 +4,14 @@ public class Pages {
     public static final String ALERTS_SELENIUM_UI = "https://seleniumui.moderntester.pl/alerts.php";
     public static final String IFRAME_SELENIUM_UI = "https://seleniumui.moderntester.pl/iframes.php";
     public static final String TABLES_SELENIUM_UI = "https://seleniumui.moderntester.pl/table.php";
+
+    public static final String ACCORDION_WIDGETS_SELENIUM_UI = "https://seleniumui.moderntester.pl/accordion.php";
+    public static final String AUTOCOMPLETE_WIDGETS_SELENIUM_UI = "https://seleniumui.moderntester.pl/autocomplete.php";
+    public static final String DATEPICKER_WIDGETS_SELENIUM_UI = "https://seleniumui.moderntester.pl/datepicker.php";
+    public static final String MENU_WIDGETS_SELENIUM_UI = "https://seleniumui.moderntester.pl/menu-item.php";
+    public static final String MODAL_DIALOG_WIDGETS_SELENIUM_UI = "https://seleniumui.moderntester.pl/modal-dialog.php";
+    public static final String PROGRESSBAR_WIDGETS_SELENIUM_UI = "https://seleniumui.moderntester.pl/progressbar.php";
+    public static final String SELECTABLE_WIDGETS_SELENIUM_UI = "https://seleniumui.moderntester.pl/selectmenu.php";
+    public static final String TOOLTIP_WIDGETS_SELENIUM_UI = "https://seleniumui.moderntester.pl/tooltip.php";
     public static final String SLIDER_WIDGETS_SELENIUM_UI = "https://seleniumui.moderntester.pl/slider.php";
 }
---
+++ src/test/java/widgets/TooltipTest.java
@@ -0,0 +1,34 @@
+package widgets;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import testbase.Pages;
+import testbase.TestBase;
+
+import java.util.List;
+
+public class TooltipTest extends TestBase {
+
+    private static final Logger logger = LoggerFactory.getLogger(SliderTest.class);
+
+    @Test
+    @Tag("widgets")
+    @Tag("tooltip")
+    void printMessage() {
+        driver.get(Pages.TOOLTIP_WIDGETS_SELENIUM_UI);
+        logger.info("Została otworzona strona: " + Pages.TOOLTIP_WIDGETS_SELENIUM_UI);
+
+        List<WebElement> paragraphs = driver.findElements(By.cssSelector("[class=\"content tooltips\"] p"));
+
+        for (WebElement paragraph : paragraphs) {
+            if (!paragraph.findElements(By.xpath("./a")).isEmpty()) {
+                WebElement infotip = paragraph.findElement(By.xpath("./a"));
+                System.out.println(infotip.getAttribute("title"));
+            }
+        }
+    }
+}
