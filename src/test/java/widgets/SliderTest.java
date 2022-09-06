16b76f9d61a175d0350e4e7341fc11fbd10aa3be
msobocinska <msobocinska@sii.pl>
2022-09-06 22:46:51 +0200

    slider refactor

--- src/test/java/widgets/SliderTest.java
+++ src/test/java/widgets/SliderTest.java
@@ -1,14 +1,13 @@
 package widgets;
 
 import org.junit.jupiter.api.Tag;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import testbase.Pages;
+import pages.Pages;
 import testbase.TestBase;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -17,11 +16,18 @@ public class SliderTest extends TestBase {
 
     private static final Logger logger = LoggerFactory.getLogger(SliderTest.class);
 
-    @ParameterizedTest
-    @ValueSource(ints = {50})
+    @Test
     @Tag("widgets")
     @Tag("slider")
-    void checkCorrectValueAfterShiftSlider50(int value) {
+    void checkCorrectValueAfterShiftSlider() {
+        moveSlider(50);
+        moveSlider(80);
+        moveSlider(80);
+        moveSlider(20);
+        moveSlider(0);
+    }
+
+    private void moveSlider(int value) {
         driver.get(Pages.SLIDER_WIDGETS_SELENIUM_UI);
         logger.info("Została otworzona strona: " + Pages.SLIDER_WIDGETS_SELENIUM_UI);
 
@@ -32,65 +38,6 @@ public class SliderTest extends TestBase {
         }
         logger.info("Slider został przesunięty " + value + " razy.");
         logger.info("Tekst na stronie: " + handle.getText() + " | Tekst oczekiwany: " + value);
-
         assertThat(Integer.parseInt(handle.getText())).isEqualTo(value);
     }
-
-    @ParameterizedTest
-    @ValueSource(ints = {80})
-    @Tag("widgets")
-    @Tag("slider")
-    void checkCorrectValueAfterShiftSlider80(int value) {
-        driver.get(Pages.SLIDER_WIDGETS_SELENIUM_UI);
-        logger.info("Została otworzona strona: " + Pages.SLIDER_WIDGETS_SELENIUM_UI);
-
-        WebElement handle = driver.findElement(By.cssSelector("#custom-handle"));
-
-        for (int i = 0; i < value; i++) {
-            handle.sendKeys(Keys.ARROW_RIGHT);
-        }
-        logger.info("Slider został przesunięty " + value + " razy.");
-        logger.info("Tekst na stronie: " + handle.getText() + " | Tekst oczekiwany: " + value);
-
-        assertThat(Integer.parseInt(handle.getText())).isEqualTo(value);
-    }
-
-    @ParameterizedTest
-    @ValueSource(ints = {20})
-    @Tag("widgets")
-    @Tag("slider")
-    void checkCorrectValueAfterShiftSlider20(int value) {
-        driver.get(Pages.SLIDER_WIDGETS_SELENIUM_UI);
-        logger.info("Została otworzona strona: " + Pages.SLIDER_WIDGETS_SELENIUM_UI);
-
-        WebElement handle = driver.findElement(By.cssSelector("#custom-handle"));
-
-        for (int i = 0; i < value; i++) {
-            handle.sendKeys(Keys.ARROW_RIGHT);
-        }
-        logger.info("Slider został przesunięty " + value + " razy.");
-        logger.info("Tekst na stronie: " + handle.getText() + " | Tekst oczekiwany: " + value);
-
-        assertThat(Integer.parseInt(handle.getText())).isEqualTo(value);
-    }
-
-    @ParameterizedTest
-    @ValueSource(ints = {0})
-    @Tag("widgets")
-    @Tag("slider")
-    void checkCorrectValueAfterShiftSlider0(int value) {
-        driver.get(Pages.SLIDER_WIDGETS_SELENIUM_UI);
-        logger.info("Została otworzona strona: " + Pages.SLIDER_WIDGETS_SELENIUM_UI);
-
-        WebElement handle = driver.findElement(By.cssSelector("#custom-handle"));
-
-        for (int i = 0; i < value; i++) {
-            handle.sendKeys(Keys.ARROW_RIGHT);
-        }
-        logger.info("Slider został przesunięty " + value + " razy.");
-        logger.info("Tekst na stronie: " + handle.getText() + " | Tekst oczekiwany: " + value);
-
-        assertThat(Integer.parseInt(handle.getText())).isEqualTo(value);
-    }
-
 }
